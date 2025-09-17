70a2bb789231569bbedf4929cc92b9f3676695c3
JadenG <jgriffith@brooklynsteamcenter.org>
2025-09-17 10:18:48 -0400

    New

---
+++ 04_Conditional_Calisthenics.py
@@ -0,0 +1,48 @@
+'''
+Exercise 1:
+Check if an integer is greater than 10.
+Return True if conditions are met, False otherwise.
+'''
+Lets_check = int(input("Place Your Number here."))
+if Lets_check > 10:
+    print("This True")
+else:
+    print("This Flase")
+    # I Made a Varible for user input then made a if else statment finding out if the user input is greater than 10 or less than.
+'''
+Exercise 2:
+Determine the ticket price based on age and student status.
+Price is $10 if under 18 or a student, $20 otherwise.
+'''
+Age_Entry = int(input("Enter Your Age"))
+if Age_Entry == 18:
+    print("The price is 20$ for entry")
+else:
+    print("The Price is 10$ for entry")
+# I made a interger input for age then writing an if statment telling it what to print for each age.
+'''
+Exercise 3:
+Prompt the user to enter a fruit name. Check if the fruit is in the list. 
+If it is, print "Yes, that fruit is in the list." 
+If it's not, print "No, that fruit is not in the list."
+'''
+From_JadenGs_FruitBasket = (input("Enter A fruit from my fruit basket"))
+
+
+'''
+Exercise 4:
+Check if a year is a century year and a leap year.
+'''
+
+'''
+Exercise 5:
+Calculate the cost of shipping for an online order based on the order weight and destination zone. 
+The shipping cost is $5 per kilogram for Zone A and $7 per kilogram for Zone B. 
+If the order weight is less than 0 kg, return an error message.
+'''
+
+'''
+Exercise 6:
+Determine the type of a triangle based on side lengths.
+Equilateral, Isosceles, Scalene, or Not a triangle.
+'''
