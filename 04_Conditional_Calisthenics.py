81ca37f3b328f5c85a5fd87c14e6546e2742d352
JadenG <jgriffith@brooklynsteamcenter.org>
2025-09-17 14:59:43 -0400

    please?

--- 04_Conditional_Calisthenics.py
+++ 04_Conditional_Calisthenics.py
@@ -26,8 +26,13 @@ Prompt the user to enter a fruit name. Check if the fruit is in the list.
 If it is, print "Yes, that fruit is in the list." 
 If it's not, print "No, that fruit is not in the list."
 '''
-From_JadenGs_FruitBasket = (input("Enter A fruit from my fruit basket"))
-
+From_JadenGs_FruitBasket = (input("Enter A fruit from my fruit basket")).strip().upper()
+JadenGsfruit_Basket = ["butter-butter no mi","chop-chop No mi","grapefruit"]
+print(JadenGsfruit_Basket)
+if From_JadenGs_FruitBasket in From_JadenGs_FruitBasket:
+    print("Yes This fruit is in the basket.")
+else:
+    print("No this fruit is not here in my basket.")
 
 '''
 Exercise 4:
@@ -40,7 +45,19 @@ Calculate the cost of shipping for an online order based on the order weight and
 The shipping cost is $5 per kilogram for Zone A and $7 per kilogram for Zone B. 
 If the order weight is less than 0 kg, return an error message.
 '''
-
+How_Heavy = float(input("Enter the order weight in kg: "))
+Where_is_it = input("Enter the destination zone (Z or E): ").strip().upper()
+if How_Heavy < 0:
+    print("Weight less than 0 kg")
+else:
+    if Where_is_it == "Z":
+        cost = How_Heavy * 5
+        print(f"Shipping to Zone Z: ${cost:.2f}")
+    elif Where_is_it == "E":
+        cost = How_Heavy * 7
+        print(f"Shipping to Zone E: ${cost:.2f}")
+    else:
+        print(" Not a Zone Invalid zone")
 '''
 Exercise 6:
 Determine the type of a triangle based on side lengths.
